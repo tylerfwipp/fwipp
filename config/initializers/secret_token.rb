089973095a1467bab30dc32095f0a6ad94302e94
Tyler Rolfe <info@tylermrolfe.com>
2013-02-02 00:00:46 -0500

    created statuses

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fwipp::Application.config.secret_token = '589320ae769a81505e8773dcfb19719fec7fa702c28a43b7cb509e84da4e2c70ef00d1372c284535da8d52dc415ffac8f8ea74757f4f84ff48878126b8e19142'
